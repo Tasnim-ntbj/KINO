58d6f34ff736d7095ed6df5a9629b5e323bac989
maimunarashid <rashidromana05@gmail.com>
2025-12-08 19:49:35 +0600

    initial design

---
+++ KINO/Makeup/productCategory.js
@@ -0,0 +1,68 @@
+//MakeUP section - product Categories
+const loadProductCategories = () => {
+  fetch("productCategory.json")
+    .then(response => response.json())
+    .then(data => displayProductCategories(data.categories))
+}
+
+// const displayProductCategories = (categories) => {
+//   const productCategories = document.getElementById("product-categories");
+//   productCategories.innerHTML = "";
+
+//   // Category Heading create
+//   // const categoryHeading = document.createElement("h1");
+//   // categoryHeading.innerText = "Product Categories";
+//   // categoryHeading.classList.add("text-2xl", "font-bold", "mb-2");
+//   // productCategories.appendChild(categoryHeading);
+
+
+//   categories.forEach(category => {
+//     const categoryContent = document.createElement("div");
+//     categoryContent.innerHTML = `<button id="category-based-product-${category.id}" onclick="loadCategoryBasedProduct(${category.id})" class="hover:bg-[#0f0d0e] hover:text-white p-2 rounded-lg w-full text-left">${category.category_name}</button>`;
+
+//     productCategories.appendChild(categoryContent);
+//   })
+
+// }
+// loadProductCategories();
+
+
+
+
+// all make up products
+const loadAllMakeupProducts = () => {
+  fetch("allProducts.json")
+    .then(response => response.json())
+    .then(data => displayAllMakeupProducts(data.allMakeupProducts))
+}
+
+const displayAllMakeupProducts = (allProducts) => {
+  const allMakeupProducts = document.getElementById("allMakeup-products");
+  allMakeupProducts.innerHTML = "";
+
+  allProducts.forEach(product => {
+    const productCard = document.createElement("div");
+    productCard.innerHTML = `<div class="card bg-base-100 w-96 flex flex-col flex-grow h-full min-h-[400px] justify-between transition-transform duration-300 ease-in-out hover:-translate-y-1 hover:shadow-lg">
+    <img class="h-[260px] pl-5 pr-5 pt-5" src="${product.image}">
+  <div class="card-body mb-[10px]">
+    <h2 class="card-title">${product.name}</h2>
+    <p>${product.description}</p>
+    <div class="flex justify-between">
+    <button class="btn rounded-[10px]">৳${product.price}</button>
+      <button class="btn rounded-2 text-white border-0 bg-[#0f0e0e]">Add to Cart</button>
+    </div>
+  </div>
+</div>`;
+
+    allMakeupProducts.appendChild(productCard);
+  })
+}
+loadAllMakeupProducts();
+
+
+// category based products
+const loadCategoryBasedProduct = (id) => {
+  const url = `allProducts.json`
+  fetch()
+
+}
